321f78ee2f16a0194fb92b50868e5fa36c550a80
ei <ei.email.com>
2017-03-05 21:52:39 +0900

    buttonを作成

---
+++ AngularJS/20160305_ngbutton/myscript.js
@@ -0,0 +1,16 @@
+angular.module('myApp', [])
+  .controller('myController', ['$scope', function($scope) {
+    $scope.users = [
+      {"name":"tanaka", "score":33.25},
+      {"name":"yamada", "score":90.62},
+      {"name":"hashimoto", "score":63},
+      {"name":"tada", "score":62},
+      {"name":"taira", "score":90.90},
+      {"name":"yamamoto", "score":12.25},
+      {"name":"hayashi", "score":99.99},
+    ];
+
+    $scope.hello = function() {
+      alert('Hello ' + $scope.user.input);
+    };
+  }]);
