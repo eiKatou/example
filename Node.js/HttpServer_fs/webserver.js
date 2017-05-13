ed43a5b00c5ccb0929cf50a4b4b0def735dc908c
ei <ei.email.com>
2017-05-13 21:33:18 +0900

    間違いを修正

--- Node.js/HttpServer_fs/webserver.js
+++ Node.js/HttpServer_fs/webserver.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 http.createServer(function (req, res) {
 
     fs.readFile('hello.html', 'UTF-8', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
     })
