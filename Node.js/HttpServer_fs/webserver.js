6aa8ac7e157268f8cce5676ad3f7e6e496fdec7c
ei <ei.email.com>
2017-05-13 21:17:40 +0900

    fsのサンプルを追加

---
+++ Node.js/HttpServer_fs/webserver.js
@@ -0,0 +1,17 @@
+// httpモジュールを読み込み、インスタンスを生成
+var http = require('http');
+var fs = require('fs');
+
+// HTTPサーバーのイベントハンドラを定義
+http.createServer(function (req, res) {
+
+    fs.readFile('hello.html', 'UTF-8', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(data);
+        res.end();
+    })
+
+
+}).listen(1337, '127.0.0.1'); // 127.0.0.1の1337番ポートで待機
+
+// 実行は $ node webserver.js
