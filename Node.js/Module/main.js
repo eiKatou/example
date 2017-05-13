ac711e9062e79cb534a0386c588db6a15227bed2
ei <ei.email.com>
2017-05-13 22:01:59 +0900

    モジュールを使う

---
+++ Node.js/Module/main.js
@@ -0,0 +1,8 @@
+var func = require('./module');
+
+var a = 1;
+var b = 2;
+console.log(a+b);
+
+console.log(func.add(10,30));
+console.log(func.sub(1, 55));
---
+++ Node.js/Module/module.js
@@ -0,0 +1,6 @@
+exports.add = function(v1, v2) {
+  return v1 + v2;
+}
+exports.sub = function(v1, v2) {
+  return v1 - v2;
+}
