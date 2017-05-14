e2f9026bb1b51daede4d390a4cedafb9289e4307
ei <ei.email.com>
2017-05-14 15:45:58 +0900

    アロー演算子のサンプルを追加

---
+++ ES6/Arrow/Hello2.js
@@ -0,0 +1,21 @@
+console.log("hello promise");
+
+function getPromise() {
+  return new Promise((resolve, reject) => {
+    console.log("時間がかかる処理開始");
+    setTimeout(() => {
+      console.log("時間がかかる処理完了");
+      resolve("OK");
+    }, 1000);
+  });
+}
+
+// getPromise().then(value => {
+//   console.log(value);
+// }).catch(error => {
+//     console.error(error);
+// });
+
+getPromise()
+  .then(value => console.log(value))
+  .catch(error => console.error(error));
