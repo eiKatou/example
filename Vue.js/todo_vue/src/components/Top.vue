f8f74f9e9d995fb93fc691449290037a3185bd63
ei <ei.email.com>
2017-06-04 21:51:21 +0900

    スタイルを設定

--- Vue.js/todo_vue/src/components/Top.vue
+++ Vue.js/todo_vue/src/components/Top.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h1>TODO一覧</h1>
-    <template v-for="(item, index) in todos" >
-      <span>
-        {{ item }}  
-        <router-link :to="{ name:'edit', params:{index:index} }">edit</router-link><br/>
-      </span>
-    </template>
+    <div id="todo_list">
+      <table id="todo_table">
+      <template v-for="(item, index) in todos" >
+        <tr class="todo_row">
+          <td class="todo">{{ item }}</td>
+          <td><router-link :to="{ name:'edit', params:{index:index} }">edit</router-link></td>
+        </tr>
+      </template>
+      </table>
+    </div>
+    <br>
     <input type="button" value="新規作成" @click="goNewTask()">
   </div>
 </template>
@@ -33,4 +38,22 @@ export default {
 </script>
 
 <style scoped>
+#todo_list {
+  width: 40%;
+  margin: auto;
+}
+#todo_table {
+  width: 100%;
+  border-collapse:collapse;
+}
+.todo_row {
+  border-bottom:1.5px #aaa solid;
+}
+.todo {
+  text-align: left;
+}
+.edit_link {
+}
+.router-link-active {
+}
 </style>
