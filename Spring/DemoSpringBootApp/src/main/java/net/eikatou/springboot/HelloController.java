cd54fa7e24d58316cb873aa2be59c87f519792ec
かとう えい <n17920517@gmail.com>
2017-09-24 20:50:00 +0900

    thymeleafを使う

--- Spring/DemoSpringBootApp/src/main/java/net/eikatou/springboot/HelloController.java
+++ Spring/DemoSpringBootApp/src/main/java/net/eikatou/springboot/HelloController.java
@@ -1,12 +1,27 @@
 package net.eikatou.springboot;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
-	@RequestMapping("/")
-	public String index() {
-		return "Hello SprintBoot.";
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "input your name:");
+		return mav;
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("name")String name,
+			ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "Hello " + name + " !");
+		mav.addObject("value", name);
+		return mav;
 	}
 }
