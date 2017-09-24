6528c55f6a4ec6525da1b6078d00f06ad196e9f8
かとう えい <n17920517@gmail.com>
2017-09-24 21:14:15 +0900

    ログを出力できるようにした

--- Spring/DemoSpringBootApp/README.md
+++ Spring/DemoSpringBootApp/README.md
@@ -1,9 +1,11 @@
 # 実行方法
-[Run As] ＞ [Spring Boot App]
+[Run As] ＞ [Spring Boot App]  
 http://localhost:8080/
 
 # ビルド方法
 [Run As] > [6 Maven build] > Goalsに「package」を入力 > [Run]ボタン押下
 
 # 参考
-http://www.techscore.com/blog/2016/11/25/start-with-sts-and-spring-boot-2/
+- http://www.techscore.com/blog/2016/11/25/start-with-sts-and-spring-boot-2/
+- http://www.techscore.com/blog/2016/12/03/start-with-sts-and-spring-boot-3/
+- http://dev.classmethod.jp/server-side/java/spring-boot_log_output/
--- Spring/DemoSpringBootApp/src/main/java/net/eikatou/springboot/HelloController.java
+++ Spring/DemoSpringBootApp/src/main/java/net/eikatou/springboot/HelloController.java
@@ -1,5 +1,7 @@
 package net.eikatou.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
+		logger.info("Called index.");
 		mav.setViewName("index");
 		mav.addObject("msg", "input your name:");
 		return mav;
@@ -19,6 +23,8 @@ public class HelloController {
 	@RequestMapping(value="/", method=RequestMethod.POST)
 	public ModelAndView send(@RequestParam("name")String name,
 			ModelAndView mav) {
+		logger.info("Called send.");
+		logger.error("Error send.");
 		mav.setViewName("index");
 		mav.addObject("msg", "Hello " + name + " !");
 		mav.addObject("value", name);
