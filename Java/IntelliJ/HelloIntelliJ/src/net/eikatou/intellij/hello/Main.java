27fffa8a081ecc6bf2c65cb036bf731959e06887
かとう えい <n17920517@gmail.com>
2017-12-24 16:34:21 +0900

    初めてのIntelliJ

---
+++ Java/IntelliJ/HelloIntelliJ/src/net/eikatou/intellij/hello/Main.java
@@ -0,0 +1,13 @@
+package net.eikatou.intellij.hello;
+
+import net.eikatou.intellij.hello.domain.Animal;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+
+	    Animal animal = new Animal();
+	    animal.nake();
+    }
+}
---
+++ Java/IntelliJ/HelloIntelliJ/src/net/eikatou/intellij/hello/domain/Animal.java
@@ -0,0 +1,9 @@
+package net.eikatou.intellij.hello.domain;
+
+public class Animal {
+    private String name;
+
+    public void nake() {
+        System.out.println("na--!");
+    }
+}
