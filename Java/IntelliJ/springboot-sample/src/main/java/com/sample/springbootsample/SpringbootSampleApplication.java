74f97a3758cab607fd7b3baf466cf2a74914d44d
かとう えい <n17920517@gmail.com>
2017-12-29 06:30:55 +0900

    SPRING INITIALIZR http://start.spring.io で生成した内容を上書き

---
+++ Java/IntelliJ/springboot-sample/src/main/java/com/sample/springbootsample/SpringbootSampleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSampleApplication.class, args);
+	}
+}
