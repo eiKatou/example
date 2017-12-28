bc2cf3d74b89762b78cc9d1732e3bf3b679382be
かとう えい <n17920517@gmail.com>
2017-12-29 06:49:27 +0900

    HelloControllerを追加

---
+++ Java/IntelliJ/springboot-sample/src/main/java/com/sample/springbootsample/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.sample.springbootsample.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello world";
+    }
+
+}
