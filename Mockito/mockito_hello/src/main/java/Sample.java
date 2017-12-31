422bcb15fa8e7a3669e6e0e5a76e367b42aeb75c
かとう えい <n17920517@gmail.com>
2017-12-31 16:14:47 +0900

    Mockitoのサンプルプロジェクトを初期登録

---
+++ Mockito/mockito_hello/src/main/java/Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+        System.out.println(sample.getMessage());
+    }
+
+    public String getMessage() {
+        return "Sample!";
+    }
+}
