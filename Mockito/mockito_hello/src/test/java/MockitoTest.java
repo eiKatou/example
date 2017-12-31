d4f0a00f134f48ec7085fab6786c50a4f3224b1e
かとう えい <n17920517@gmail.com>
2017-12-31 16:51:26 +0900

    Mockitoのサンプルコードを追加 参考：http://etc9.hatenablog.com/entry/20101108/1289218176

--- Mockito/mockito_hello/src/test/java/MockitoTest.java
+++ Mockito/mockito_hello/src/test/java/MockitoTest.java
@@ -1,8 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class MockitoTest {
-    @Test
-    public void test() {
+    @Mock
+    Person mockPerson;
+    @InjectMocks
+    Sample sample = new Sample(mockPerson);
 
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void 正しい返事が返って来る() {
+        when(mockPerson.name()).thenReturn("yamada hanako");
+        String greet = sample.greet();
+        assertThat(greet).isEqualTo("Hello, yamada hanako!!");
     }
 }
