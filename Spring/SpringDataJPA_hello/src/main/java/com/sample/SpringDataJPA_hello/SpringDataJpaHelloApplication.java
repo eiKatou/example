ce91899832e8fab4598c57e287fcd7a271e7bb61
かとう えい <n17920517@gmail.com>
2018-01-02 06:56:32 +0900

    プロジェクトの初期登録。 以下のサイトで、Web, JPA, H2, Lombokを選択。 http://start.spring.io

---
+++ Spring/SpringDataJPA_hello/src/main/java/com/sample/SpringDataJPA_hello/SpringDataJpaHelloApplication.java
@@ -0,0 +1,12 @@
+package com.sample.SpringDataJPA_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaHelloApplication.class, args);
+	}
+}
