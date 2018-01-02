0ce7aff28c782da76a31114030243dafeaa7e3cf
かとう えい <n17920517@gmail.com>
2018-01-02 16:06:38 +0900

    DBの読み取りをページ単位で実施した時に取得できるデータを表示 参考：http://yamkazu.hatenablog.com/entry/20111013/1318490116

--- Spring/SpringDataJPA_hello/src/main/java/com/sample/SpringDataJPA_hello/controller/PageController.java
+++ Spring/SpringDataJPA_hello/src/main/java/com/sample/SpringDataJPA_hello/controller/PageController.java
@@ -1,8 +1,10 @@
 package com.sample.SpringDataJPA_hello.controller;
 
+import com.sample.SpringDataJPA_hello.entity.Customer;
 import com.sample.SpringDataJPA_hello.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,15 @@ public class PageController {
     public String index() {
         System.out.println("page=0, size=2");
         Pageable pageable1 = PageRequest.of(0, 2);
-        customerRepository.findAll(pageable1)
-                .forEach(System.out::println);
+        Page<Customer> page = customerRepository.findAll(pageable1);
+        page.forEach(System.out::println);
+
+        int number = page.getNumber(); // ページ番号を取得
+        int numberOfElements = page.getNumberOfElements(); // 取得したページの要素数
+        int size = page.getSize(); // 指定したページの要素数
+        long totalElements = page.getTotalElements(); // 全件の数を取得
+        int totalPages = page.getTotalPages(); // ページ数を取得
+        System.out.println(String.format("number=%d, numberOfElements=%d, size=%d, totalElements=%d, totalPages=%d", number, numberOfElements, size, totalElements, totalPages));
 
         System.out.println("page=1, size=2");
         Pageable pageable2 = PageRequest.of(1, 2);
