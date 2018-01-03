ed8b0a6fb965def5ee7b289ef76f9c8b7feb3ccb
かとう えい <n17920517@gmail.com>
2018-01-04 08:54:41 +0900

    REST Webサービスの開発 APIを追加 参考：はじめてのSpring Boot P.81-P.86

--- Spring/RESTWebService/README.md
+++ Spring/RESTWebService/README.md
@@ -3,11 +3,29 @@
 # API
 
 ## 顧客一覧の取得
+```bash
 curl http://localhost:8080/api/customers -v -X GET
+```
 
 ## 顧客の取得
+```bash
 curl http://localhost:8080/api/customers/1 -v -X GET
+```
 
+## 顧客の新規作成
+```bash
+curl http://localhost:8080/api/customers -v -X POST -H "Content-Type: application/json" -d "{\"firstName\":\"Tamako\",\"lastName\":\"Nobi\"}" 
+```
+
+## 顧客の1件更新
+```bash
+curl http://localhost:8080/api/customers/1 -v -X PUT -H "Content-Type: application/json" -d "{\"firstName\":\"Nobio\",\"lastName\":\"Nobi\"}" 
+```
+
+## 顧客の削除
+```bash
+curl http://localhost:8080/api/customers/1 -v -X DELETE 
+```
 
 # その他
 
--- Spring/RESTWebService/src/main/java/com/sample/rest/api/CustomerRestController.java
+++ Spring/RESTWebService/src/main/java/com/sample/rest/api/CustomerRestController.java
@@ -3,10 +3,8 @@ package com.sample.rest.api;
 import com.sample.rest.entity.Customer;
 import com.sample.rest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,23 @@ public class CustomerRestController {
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     Customer getCustomer(@PathVariable Integer id) {
         return customerService.findOne(id).orElse(null);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    Customer postCustomer(@RequestBody Customer customer) {
+        return customerService.create(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return customerService.update(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteCustomer(@PathVariable Integer id) {
+        customerService.delete(id);
     }
 }
