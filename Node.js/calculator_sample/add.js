d3d3b6345a439ac399af8c9fc2afaf9cf7d40042
かとう えい <n17920517@gmail.com>
2018-05-26 10:09:03 +0900

    Node.js よくある計算サンプルを追加

---
+++ Node.js/calculator_sample/add.js
@@ -0,0 +1,7 @@
+if(process.argv.length < 4) {
+  console.log("usage: $node add.js 1 2");
+}
+
+const num1 = parseInt(process.argv[2], 10);
+const num2 = parseInt(process.argv[3], 10);
+console.log(num1 + num2);
