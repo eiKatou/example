9715b93f255883a611ada0e941218af618c1e81c
かとう えい <n17920517@gmail.com>
2018-05-26 21:23:39 +0900

    計算結果を表示できるようにした

--- Express/calc_webapp/routes/calc.js
+++ Express/calc_webapp/routes/calc.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('calc', { pageName: 'Calc' });
+  res.render('calc', { pageName: 'Calc', result: ''});
+});
+
+router.post('/add', function(req, res) {
+  console.log(req.body);
+  console.log(req.body.num1);
+  console.log(req.body.num2);
+  const result = parseInt(req.body.num1, 10) + parseInt(req.body.num2, 10);
+  // res.send('POST request to the homepage');
+  res.render('calc', { pageName: 'Calc' , result: result});
 });
 
 module.exports = router;
