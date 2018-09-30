babbfdc3b5c0cf59bd778fdb50aa0b41532602ca
かとう えい <n17920517@gmail.com>
2018-09-30 21:03:55 +0900

    AWS LambdaをGoで実行

---
+++ Go/aws_lambda/hello.go
@@ -0,0 +1,24 @@
+package main
+
+// https://dev.classmethod.jp/cloud/aws/aws-lambda-supports-go/
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"What is your name?"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+func hello(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("Hello %s!!", event.Name)}, nil
+}
+
+func main() {
+	lambda.Start(hello)
+}
