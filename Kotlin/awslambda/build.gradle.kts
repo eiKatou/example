5f62dc81b9b79554bafc6c4790291e17c22622a6
かとう えい <n17920517@gmail.com>
2020-10-24 08:06:37 +0900

    初期コミット

---
+++ Kotlin/awslambda/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+application {
+    mainClassName = "MainKt"
+}
