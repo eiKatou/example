b94bdade7fa2da50ec5e1fe222d12d2bbbc385fc
かとう えい <n17920517@gmail.com>
2020-11-07 07:11:48 +0900

    初期インポート

---
+++ Kotlin/Coroutine/build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+group = "net.eikatou"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+application {
+    mainClassName = "MainKt"
+}
