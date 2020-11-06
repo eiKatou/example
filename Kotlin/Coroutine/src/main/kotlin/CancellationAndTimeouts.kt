c05516bf0692ecf9eace21121b5ea2b6719dfd16
かとう えい <n17920517@gmail.com>
2020-11-07 08:10:15 +0900

    cancelの1つ目まで実施

---
+++ Kotlin/Coroutine/src/main/kotlin/CancellationAndTimeouts.kt
@@ -0,0 +1,34 @@
+import kotlinx.coroutines.*
+
+fun main(args: Array<String>) {
+//    println("Hello")
+//    cancel1()
+    cancel1a()
+}
+
+fun cancel1() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("job: I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancel() // cancels the job
+    job.join() // waits for job's completion
+    println("main: Now I can quit.")
+}
+
+fun cancel1a() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("job: I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin()
+    println("main: Now I can quit.")
+}
