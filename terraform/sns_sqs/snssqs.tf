a0a2e0f6dd21da0127df055f08641550d4e3fab9
かとう えい <n17920517@gmail.com>
2020-11-28 20:31:38 +0900

    policyをええ感じに修正 outputの誤りを修正

--- terraform/sns_sqs/snssqs.tf
+++ terraform/sns_sqs/snssqs.tf
@@ -18,31 +18,41 @@ resource "aws_sqs_queue" "main_queue" {
   }
 }
 
+data "aws_iam_policy_document" "sqs-queue-policy" {
+  policy_id = "sqs_policy"
+
+  statement {
+    sid    = "First"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "SQS:SendMessage",
+    ]
+
+    resources = [
+      aws_sqs_queue.main_queue.arn,
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        aws_sns_topic.main_topic.arn,
+      ]
+    }
+  }
+}
+
 # 全ての権限のpolicyを与える
 resource "aws_sqs_queue_policy" "sqs_queue_policy" {
   queue_url = aws_sqs_queue.main_queue.id
-  policy    = <<EOF
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "SQS:SendMessage",
-      "Resource": "${aws_sqs_queue.main_queue.arn}",
-      "Condition": {
-        "ArnLike": {
-          "aws:SourceArn": "${aws_sns_topic.main_topic.arn}"
-        }
-      }
-    }
-  ]
-}
-EOF
+  policy    = data.aws_iam_policy_document.sqs-queue-policy.json
 }
 
 # SNS Subscription
@@ -53,9 +63,9 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   endpoint  = aws_sqs_queue.main_queue.arn
 }
 
-output "aws_sns_url" {
+output "aws_sqs_url" {
   value = aws_sqs_queue.main_queue.id
 }
-output "aws_sqs_url" {
+output "aws_sns_arn" {
   value = aws_sns_topic.main_topic.id
 }
