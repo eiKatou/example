142468e3b6f2207ba344e14cfd2e4eaabc163fa0
かとう えい <n17920517@gmail.com>
2020-11-29 15:52:31 +0900

    Kotlinファイル追加

---
+++ Saga/SagaBookTravel/src/main/kotlin/TravelAgentService.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Travel Agent Service")
+
+    // TODO: 定期的にSQSを見て、順にサービスを実行
+}
+
+class TravelAgentService {
+}
---
+++ Saga/SagaBookTravel/src/main/kotlin/client.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    println("Cleint")
+
+    // TODO: SNSにリクエストを定期的に投げる
+}
