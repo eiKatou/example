05367f0885d5f81f1ada0dffef5ff817d69a2111
かとう えい <n17920517@gmail.com>
2020-12-12 22:37:16 +0900

    lifecycle ignore_changesを指定

--- terraform/aurora_mysql/rds.tf
+++ terraform/aurora_mysql/rds.tf
@@ -16,16 +16,22 @@ resource "aws_db_subnet_group" "private-db" {
 # Aurora
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster
 resource "aws_rds_cluster" "default" {
-  cluster_identifier     = "aurora-cluster-demo"
-  engine                 = "aurora-mysql"
-  engine_version         = "5.7.mysql_aurora.2.03.2"
-  availability_zones     = ["us-west-2b", "us-west-2c"]
+  cluster_identifier = "aurora-cluster-demo"
+  engine             = "aurora-mysql"
+  engine_version     = "5.7.mysql_aurora.2.03.2"
+  //  availability_zones     = ["us-west-2b", "us-west-2c"]
   database_name          = "mydb"
   master_username        = "foo"
   master_password        = "foobarbaz"
   vpc_security_group_ids = [aws_security_group.private-db-sg.id]
   db_subnet_group_name   = aws_db_subnet_group.private-db.name
   skip_final_snapshot    = true
+  lifecycle {
+    ignore_changes = [
+      master_password,
+      availability_zones
+    ]
+  }
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster_instance
 resource "aws_rds_cluster_instance" "cluster_instances" {
