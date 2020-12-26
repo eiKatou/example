668abface63e822332494592ad2a06f7522afb98
かとう えい <n17920517@gmail.com>
2020-12-26 19:14:28 +0900

    不要なクラスを消してまとめた

--- Spring/SQSApp/src/main/kotlin/com/example/demo/HelloController.kt
+++ Spring/SQSApp/src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,6 +1,8 @@
 package com.example.demo
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("api/hello")
 class HelloController {
     @Autowired
-    lateinit var sqsQueueSender: SqsQueueSender
+    @Qualifier("sqsAppQueueMessaging")
+    lateinit var sqsAppQueueMessaging: QueueMessagingTemplate
 
     @RequestMapping(method = [RequestMethod.GET])
     fun hello() = "hello world."
@@ -16,7 +19,7 @@ class HelloController {
     @RequestMapping(method = [RequestMethod.POST])
     @ResponseStatus(HttpStatus.CREATED)
     fun postMessage(@RequestBody message: String): String {
-        sqsQueueSender.sendString(message)
+        sqsAppQueueMessaging.convertAndSend(message)
         return "OK"
     }
 
