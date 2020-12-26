96d1f49aa1652d0d3cdb3375585245e0dc703347
かとう えい <n17920517@gmail.com>
2020-12-26 21:58:48 +0900

    deletionPolicyを設定

--- Spring/SQSApp/src/main/kotlin/com/example/demo/MessageReceiver.kt
+++ Spring/SQSApp/src/main/kotlin/com/example/demo/MessageReceiver.kt
@@ -1,12 +1,16 @@
 package com.example.demo
 
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
 import org.springframework.stereotype.Component
+import java.lang.RuntimeException
 
 @Component
 class MessageReceiver {
-    @SqsListener("sqs-app")
+    @SqsListener(value = ["sqs-app"], deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     fun queueListener(message: String?) {
         println(message)
+        // deletionPolicyの設定により、例外時はメッセージが削除されない。
+//        throw RuntimeException("test exception.")
     }
 }
